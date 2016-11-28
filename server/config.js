3edee7373110570ead43d24be6d2f84477d84dc9
Matthew French <aundmr@gmail.com>
2016-11-28 16:32:33 -0700

    Delete config.js

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PORT: 3000,
-  MONGO_URI: 'mongodb://basementboss:imdabes@ds157667.mlab.com:57667/basementboss',
-  SESSION_SECRET: 'mrfontanasayshedabesandheabasementbosssohetotallyrightdiscoinfernonutellailovefishescausetheysodeliciousgotgoldfishes'
-};
