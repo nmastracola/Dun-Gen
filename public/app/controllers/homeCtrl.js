1eea1d466cc71afad95aa0a8a0e682cdab84ee1e
Lucas Hawk <Thunderchunk@Lucass-MacBook-Pro.local>
2016-11-28 16:10:00 -0700

    loginPageDone

--- public/app/controllers/homeCtrl.js
+++ public/app/controllers/homeCtrl.js
@@ -3,5 +3,9 @@ angular.module('scribe').controller('homeCtrl', function($scope, sService){
 $scope.tester = sService.test;
 
 
+//=====================  modal  =====================
+
+$scope.toggler = false;
+
 
 });
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: 3000,
+  MONGO_URI: 'mongodb://basementboss:imdabes@ds157667.mlab.com:57667/basementboss',
+  SESSION_SECRET: 'mrfontanasayshedabesandheabasementbosssohetotallyrightdiscoinfernonutellailovefishescausetheysodeliciousgotgoldfishes'
+};
