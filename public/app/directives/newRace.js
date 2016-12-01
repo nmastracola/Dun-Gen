6c39203ac0a612e0a15c0a1baf1625e3ca164688
Joe Whitehorn <remidy@gmail.com>
2016-12-01 16:53:09 -0700

    worked on races in character creation until they are able to display the proper info depending on the race that is picked by the user.

--- public/app/directives/newRace.js
+++ public/app/directives/newRace.js
@@ -4,27 +4,38 @@ angular.module('scribe')
             restrict: 'E',
             templateUrl: './app/directives/newRace.html',
             controller: function($scope, $state) {
+                $scope.staticRace = false;
                 $scope.raceList = ['Dwarves', 'Elves', 'Gnomes', 'Half-Elves', 'Half-Orcs', 'Halflings', 'Humans'];
                 $scope.chosenRace = "Half-Elves";
                 $scope.racePicker = function(x) {
                     $scope.chosenRace = $scope.raceList[x];
                     $scope.info();
-                };
-                $scope.attributeBonuses = function() {
 
                 };
+                $scope.confirmRace = function () {
 
+                }
                 $scope.info = function() {
                     for (var i = 0; i < $scope.races.length; i++) {
                         if ($scope.races[i].name === $scope.chosenRace) {
                             $scope.raceInfo = $scope.races[i].traits;
+                            $scope.modText = $scope.races[i].modText;
+                            console.log($scope.races);
+                            var raceCounter = i;
                         };
                     };
-                    console.log($scope.races);
-                    console.log($scope.raceInfo);
+                    if (!$scope.races[raceCounter].mod.bon) {
+                        $scope.staticRace = true;
+                        for (var i = 0; i < $scope.abilityToggler.length; i++) {
+                          $scope.abilityToggler[i] = false;
+                        }
+                        console.log($scope.races[raceCounter].mod.bon);
+                    }else {
+                      $scope.staticRace = false;
+
+                    }
                 };
                 $scope.info();
-                console.log($scope.races);
 
                 //race togglers
                 $scope.raceToggler = [false, false, false, true, false, false, false];
@@ -42,9 +53,27 @@ angular.module('scribe')
                 //gender togglers
                 $scope.male = false;
                 $scope.female = false;
+
+                // SELECTED ABILITY TOGGLER
+
+                $scope.abilityToggler = [false, false, false, false, false, false];
+                $scope.abilitySwitcher = function(x) {
+
+                    for (var i = 0; i < $scope.abilityToggler.length; i++) {
+                        if (i == x) {
+                            $scope.abilityToggler[i] = true;
+                        } else {
+                            $scope.abilityToggler[i] = false;
+                        }
+                    }
+
+                };
+
             },
             scope: {
-                races: "="
+                races: "=",
+                attributes: "="
+
             },
             link: function(scope, element, attributes) {}
         };
