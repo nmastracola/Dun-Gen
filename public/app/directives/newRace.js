4b7f4f7748acb31d108e09a4ee869f75f554398c
Joe Whitehorn <remidy@gmail.com>
2016-12-01 21:16:16 -0700

    pushed user selections to character object.

--- public/app/directives/newRace.js
+++ public/app/directives/newRace.js
@@ -3,17 +3,44 @@ angular.module('scribe')
         return {
             restrict: 'E',
             templateUrl: './app/directives/newRace.html',
-            controller: function($scope, $state) {
+            controller: function($scope, $state, characterService) {
                 $scope.staticRace = false;
                 $scope.raceList = ['Dwarves', 'Elves', 'Gnomes', 'Half-Elves', 'Half-Orcs', 'Halflings', 'Humans'];
-                $scope.chosenRace = "Half-Elves";
                 $scope.racePicker = function(x) {
                     $scope.chosenRace = $scope.raceList[x];
                     $scope.info();
 
                 };
                 $scope.confirmRace = function () {
-
+                  if ($scope.races[$scope.raceCounter].mod.bon) {
+                    $scope.attributes[$scope.raceCounter]+= 2
+                  }
+                  if ($scope.races[$scope.raceCounter].mod.cha) {
+                    $scope.attributes[5]+= $scope.races[$scope.raceCounter].mod.cha
+                  }
+                  if ($scope.races[$scope.raceCounter].mod.con) {
+                    $scope.attributes[2]+= $scope.races[$scope.raceCounter].mod.con
+                  }
+                  if ($scope.races[$scope.raceCounter].mod.dex) {
+                    $scope.attributes[1]+= $scope.races[$scope.raceCounter].mod.dex
+                  }
+                  if ($scope.races[$scope.raceCounter].mod.int) {
+                    $scope.attributes[3]+= $scope.races[$scope.raceCounter].mod.int
+                  }
+                  if ($scope.races[$scope.raceCounter].mod.str) {
+                    $scope.attributes[0]+= $scope.races[$scope.raceCounter].mod.str
+                  }
+                  if ($scope.races[$scope.raceCounter].mod.wis) {
+                    $scope.attributes[4]+= $scope.races[$scope.raceCounter].mod.wis
+                  }
+                  var charCre = characterService.characterCreationObject.core
+                  characterService.characterCreationObject.static = $scope.chosenRace
+                  charCre.strength = $scope.attributes[0]
+                  charCre.dexterity = $scope.attributes[1]
+                  charCre.constitution = $scope.attributes[2]
+                  charCre.intelligence = $scope.attributes[3]
+                  charCre.wisdom = $scope.attributes[4]
+                  charCre.charisma = $scope.attributes[5]
                 }
                 $scope.info = function() {
                     for (var i = 0; i < $scope.races.length; i++) {
@@ -21,15 +48,17 @@ angular.module('scribe')
                             $scope.raceInfo = $scope.races[i].traits;
                             $scope.modText = $scope.races[i].modText;
                             console.log($scope.races);
-                            var raceCounter = i;
+                            $scope.raceCounter = i;
                         };
                     };
-                    if (!$scope.races[raceCounter].mod.bon) {
+                    if (!$scope.races[$scope.raceCounter].mod.bon) {
                         $scope.staticRace = true;
                         for (var i = 0; i < $scope.abilityToggler.length; i++) {
                           $scope.abilityToggler[i] = false;
+                          $scope.raceCounter = null;
+
                         }
-                        console.log($scope.races[raceCounter].mod.bon);
+                        console.log($scope.races[$scope.raceCounter].mod.bon);
                     }else {
                       $scope.staticRace = false;
 
@@ -72,8 +101,9 @@ angular.module('scribe')
             },
             scope: {
                 races: "=",
-                attributes: "="
-
+                attributes: "=",
+                chosenRace:"=",
+                attributes:"="
             },
             link: function(scope, element, attributes) {}
         };
