2bb53c9a4a061685a74e911f31444fd101afbf39
Kevin Fontana <kheartse@yahoo.com>
2016-12-06 16:37:54 -0700

    finished function that checks user data and adds hitpoints according to hitDie, feat, con mod and if the user has selected toughness

--- resources/empty.js
+++ resources/empty.js
@@ -1,11 +1,18 @@
 $scope.classSelected = function() {
-var class = characterService.characterCreationObject.static.classes[0].class
+var class = characterService.characterCreationObject.static.classes[0].class;
+var feats = characterService.characterCreationObject.static.feats;
 
-	for (var i = 0; i < $scope.classes; 1++) {
+
+	for (var i = 0; i < $scope.classes; i++) {
 		if ($scope.classes[i].class === class) {
-		return
-		}
+	    charHitPoints = $scope.classes[i].hitDie + 1
+
+      for (var i = 0; i < feats.length; i++) {
+        if (feats[i].value === "Toughness") {
+          charHitPoints = charHitPoints + 3
+        }
+      }
+
+    }
 	}
-$scope.charHitPoints = function() {
-	for (var i = 0; i <
-}
+$scope.charHitPoints = 0;
