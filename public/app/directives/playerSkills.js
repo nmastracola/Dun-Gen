a6d5d045175cb6da107a8105751a66dd84e01b44
Joe Whitehorn <remidy@gmail.com>
2016-12-12 07:31:39 -0700

    all but skills total works.

--- public/app/directives/playerSkills.js
+++ public/app/directives/playerSkills.js
@@ -8,25 +8,54 @@ angular.module('scribe')
         scope.skillClassToggler = true;
         scope.skillModFun = function(skill) {
           if (skill.primaryAttribute = "Str") {
-            var att = character.core.strength - 10
-            return att / 2
+            var att = scope.character.core.strength - 10
+            return Math.floor(att / 2)
           } else if (skill.primaryAttribute = "Dex") {
-            var att = character.core.dexterity - 10
-            return att / 2
+            var att = scope.character.core.dexterity - 10
+            return Math.floor(att / 2)
           } else if (skill.primaryAttribute = "Con") {
-            var att = character.core.constitution - 10
-            return att / 2
+            var att = scope.character.core.constitution - 10
+            return Math.floor(att / 2)
           } else if (skill.primaryAttribute = "Int") {
-            var att = character.core.intelligence - 10
-            return att / 2
+            var att = scope.character.core.intelligence - 10
+            return Math.floor(att / 2)
           } else if (skill.primaryAttribute = "Wis") {
-            var att = character.core.wisdom - 10
-            return att / 2
+            var att = scope.character.core.wisdom - 10
+            return Math.floor(att / 2)
           } else if (skill.primaryAttribute = "Cha") {
-            var att = character.core.charisma - 10
-            return att / 2
+            var att = scope.character.core.charisma - 10
+            return Math.floor(att / 2)
+          }
+          console.log(scope.character);
+        }
+
+        scope.ranks = 0;
+        scope.skillModifierSetter = function (skill, numToAdd) {
+          for (var i = 0; i < scope.character.skills.length; i++) {
+            if (scope.character.skills[i] === skill) {
+              if (numToAdd !== 0) {
+                if (numToAdd > 0) {
+                  scope.character.skills[i].ranks += numToAdd
+                  return scope.character.skills[i].ranks
+                }
+                if (numToAdd < 0) {
+                  scope.character.skills[i].ranks += numToAdd
+                  return scope.character.skills[i].ranks
+                }
+              }
+            }
           }
         }
+
+        scope.skillClassTogglerFun = function (skill) {
+          if (skill) {
+            return skill.classSkill
+          }
+        }
+
+
+
+
       }
     };
   });
