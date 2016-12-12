5af685a52d8368fd32f7b0ae0c3848ecc5c1c4a8
Joe Whitehorn <remidy@gmail.com>
2016-12-12 15:15:14 -0700

    Started feats HUD.

--- public/app/directives/playerFeats.js
+++ public/app/directives/playerFeats.js
@@ -1,9 +1,26 @@
 angular.module('scribe')
-.directive('playerFeats', function(){
-return{
-  restrict: 'E',
-  templateUrl: './app/directives/playerFeats.html',
-  link: function( scope, element, attributes ) {}
-};
+  .directive('playerFeats', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './app/directives/playerFeats.html',
+      link: function(scope, element, attributes) {
+        scope.playerFeats = []
 
-});
+        scope.addPlayerFeats = function() {
+            for (var i = 0; i < scope.feats.length; i++) {
+              for (var j = 0; j < scope.character.feats.length; j++) {
+                if (scope.character.feats[j].name === scope.feats[i].name) {
+                  scope.playerFeats.push(scope.feats[i])
+                }
+              }
+            }
+          }
+          // LEAVE THIS***********
+        scope.addPlayerFeats()
+          // LEAVE THIS***********
+
+
+      }
+    };
+
+  });
