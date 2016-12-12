3b5d8c33979c4528902bc0acc012e5bb6a218ec4
Kevin Fontana <kheartse@yahoo.com>
2016-12-12 11:47:11 -0700

    merging master

--- public/app/directives/playerSkillsHud.js
+++ public/app/directives/playerSkillsHud.js
@@ -3,7 +3,10 @@ angular.module('scribe')
 return{
   restrict: 'E',
   templateUrl: './app/directives/playerSkillsHud.html',
-  link: function( scope, element, attributes ) {}
+  link: function( scope, element, attributes ) {},
+  controller: function($scope, characterService) {
+    $scope
+  }
 };
 
 });
