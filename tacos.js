9a7f452b6ca4653ca3bb1067bcce086c826c4d86
thunderchunk <lucaswhawk@gmail.com>
2016-12-15 09:22:08 -0700

    setup

---
+++ tacos.js
@@ -0,0 +1,19 @@
+var artArr = [1, 2, 3, 4, 5, 6];
+var arr = [1, 2, 3];
+
+//x = 1 // -1
+
+var iterator = function(x){
+  if ((x == -1) &&(arr[0] == artArr[0])){
+    
+  }
+  else if ((x == 1) &&(arr[2] == artArr[(artArr.length -1)])){
+    
+  }
+  else if(x == 1){
+    
+  }
+  else if(x == -1){
+    
+  }
+}
