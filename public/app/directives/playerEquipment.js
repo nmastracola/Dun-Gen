a869b61d55799925ede73f5aa278cdb7fa67ffc4
Joe Whitehorn <remidy@gmail.com>
2016-12-14 21:11:05 -0700

    Equiping armor effects AC

--- public/app/directives/playerEquipment.js
+++ public/app/directives/playerEquipment.js
@@ -31,6 +31,68 @@ return{
       scope.character.equipment[0].wealth -= item.Cost
       scope.moneyConverter()
     }
+    scope.equippedArmors=[]
+    scope.equippedShields=[]
+    scope.equipArmor = function (armor) {
+      for (var i = 0; i < scope.equippedArmors.length; i++) {
+        if (scope.equippedArmors[i] === armor) {
+          scope.equippedArmors.splice(i,1)
+          console.log(scope.equippedArmors);
+          scope.acSetter()
+          return
+        }
+      }
+      if (armor.type !== "Shield") {
+        scope.equippedArmors.push(armor)
+        console.log(scope.equippedArmors);
+        scope.acSetter()
+        return
+      }
+      for (var i = 0; i < scope.equippedShields.length; i++) {
+        if (scope.equippedShields[i] === armor) {
+          scope.equippedShields.splice(i,1)
+          console.log(scope.equippedShields);
+          scope.acSetter()
+          return
+        }
+      }
+      if (armor.type === "Shield") {
+        scope.equippedShields.push(armor)
+        console.log(scope.equippedShields);
+        scope.acSetter()
+        return
+      }
+
+
+    }
+
+    scope.acSetter = function () {
+      scope.armorBonusSetter()
+      scope.shieldBonusSetter()
+    }
+
+    scope.armorBonusSetter=function () {
+      if (scope.equippedArmors[0]) {
+        scope.playerArmorBonus = scope.equippedArmors[0].ArmorShieldBonus
+      }
+      else {
+        scope.playerArmorBonus = 0
+      }
+    }
+    scope.armorBonusSetter()
+
+    scope.shieldBonusSetter=function () {
+      if (scope.equippedShields[0]) {
+        scope.playerShieldBonus = scope.equippedShields[0].ArmorShieldBonus
+      }
+      else {
+        scope.playerShieldBonus = 0
+      }
+    }
+    scope.shieldBonusSetter()
+
+
+
   }
 };
 
